7f3fd4ad42b4e87066abf6b0eb71f374a5804be9
qitas <qitas@qitas.cn>
2020-04-05 23:08:12 +0800

    fix bug

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -8,7 +8,7 @@ env = DefaultEnvironment()
 
 FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-N27")
 assert FRAMEWORK_DIR and isdir(FRAMEWORK_DIR)
-SDK_DIR = join(FRAMEWORK_DIR, "cores", "arduino", "kendryte")
+SDK_DIR = join(FRAMEWORK_DIR, "cores", "arduino", "kendryte-standalone-sdk")
 
 env.SConscript("_bare.py", exports="env")
 
